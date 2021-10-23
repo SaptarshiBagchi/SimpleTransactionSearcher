362231a121ff55c2e85e7deb0d9c2897e08fe7c0
Saptarshi <roni.bagchi@gmail.com>
2021-10-23 23:23:16 +0530

    Cleaned response

--- src/models/transaction.model.ts
+++ src/models/transaction.model.ts
@@ -11,6 +11,17 @@ const transactionSchema = new mongoose.Schema({
     transaction_note: { type: String },
     transaction_amount: { type: Number, default: 0 },
     transaction_type: { type: String, enum: ['Default', 'Medical', 'Groceries', 'Electronics'], default: 'Default' }
+}, {
+    toJSON: {
+        transform: function (doc, ret, options) {
+            ret.id = ret._id
+            const d = new Date(ret.transaction_date)
+            ret.transaction_date = d.toUTCString()
+            delete ret._id
+            delete ret.__v
+            return ret
+        }
+    }
 })
 
 transactionSchema.index({ transaction_note: 'text' })
