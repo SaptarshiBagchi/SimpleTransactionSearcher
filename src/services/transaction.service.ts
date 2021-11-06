d7f03c24bd1c031856ee8d3f9ddd981696f7d793
Saptarshi <roni.bagchi@gmail.com>
2021-11-06 21:08:55 +0530

    Removed unsed headers

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,5 +1,4 @@
 import Service from "../../baseInterfaces/service";
-import { ITransaction } from "../models/transaction.model";
 import TransactionRepository from "../repositories/transaction.repository";
 
 export class UserService extends Service {
