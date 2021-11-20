836f42ed21580fbf95b6e26b8e118827570828b9
Saptarshi <roni.bagchi@gmail.com>
2021-11-20 23:32:26 +0530

    Added responseType as a type

--- baseInterfaces/controller.ts
+++ baseInterfaces/controller.ts
@@ -1,6 +1,7 @@
 import IService from "./IService";
 import { Request, Response, NextFunction } from 'express'
 import { catchAsync } from "../lib/catchAsync";
+import { ResponseType } from "../lib/types";
 
 export default class Controller {
     service: IService;
@@ -8,18 +9,18 @@ export default class Controller {
         this.service = service;
     }
 
-    all = catchAsync(async (_1: Request, res: Response, _2: NextFunction): Promise<Response<unknown, Record<string, unknown>>> => {
+    all = catchAsync(async (_1: Request, res: Response, _2: NextFunction): Promise<ResponseType> => {
         const response = await this.service.all();
         return res.status(200).json(response);
     })
 
-    create = catchAsync(async (req: Request, res: Response, _: NextFunction): Promise<Response<unknown, Record<string, unknown>>> => {
+    create = catchAsync(async (req: Request, res: Response, _: NextFunction): Promise<ResponseType> => {
         const response = await this.service.create(req.body);
         return res.status(200).json(response);
 
     })
 
-    findText = catchAsync(async (req: Request, res: Response, _: NextFunction): Promise<Response<unknown, Record<string, unknown>>> => {
+    findText = catchAsync(async (req: Request, res: Response, _: NextFunction): Promise<ResponseType> => {
 
         const searchText = req.query.text
         if (typeof searchText === 'string') {
